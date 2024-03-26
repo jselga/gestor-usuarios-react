ac58ff6181643067c359bdefa2f5f5f02128fe43
jselga <jordi.selga@iescarelesvallbona.cat>
2024-03-26 18:46:34 +0100

    Optimizando rendimiento

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,32 @@
 import { useState } from "react";
-import useFormulario from "./hooks/useFormulario";
-import Input from "./components/Input";
+
 import Card from "./components/Card";
 import Container from "./components/Container";
-import Button from "./components/Button"
+import Button from "./components/Button";
+import UserForm from "./components/UserFrom";
 function App() {
-  const [usuarios,setUsuarios] =useState([])
-  const [formulario, handleChange,reset] = useFormulario({
-    name: "",
-    lastname: "",
-    email: "",
-  });
-  const submit = e =>{
-    
-    e.preventDefault()
-    setUsuarios([
-      ...usuarios,
-      formulario,
-    ])
-    reset()
-  }
-
+  const [usuarios, setUsuarios] = useState([]);
+  const submit = (usuario) => {
+    setUsuarios([...usuarios, usuario]);
+  };
+  console.log(usuarios)
   return (
-    <div style={{marginTop:'15%'}}>
-    <Container>
-      <Card>
-        <div style={{ padding: 20 }}>
-          <form onSubmit={submit}>
-            <Input
-              label="Nombre"
-              name="name"
-              value={formulario.name}
-              onChange={handleChange}
-              placeholder='Nombre'
-            />
-            <Input
-              label="Apellido"
-              name="lastname"
-              value={formulario.lastname}
-              onChange={handleChange}
-              placeholder='Apellido'
-            />
-            <Input
-              label="Correo"
-              name="email"
-              value={formulario.email}
-              onChange={handleChange}
-              placeholder='Correo'
-            />
-            <Button>Enviar</Button>
-
-          </form>
-        </div>
-      </Card>
-      <Card>
-        <ul>
-          {usuarios.map(u =>
-            <li key={u.email} >{`${u.name} ${u.lastname}: ${u.email}`}</li> )}
-        </ul>
-      </Card>
-    </Container>
+    <div style={{ marginTop: "15%" }}>
+      <Container>
+        <Card>
+          <div style={{ padding: 20 }}>
+            <UserForm submit={submit}></UserForm>
+          </div>
+        </Card>
+        <Card>
+          <ul>
+            {usuarios.map((u) => (
+              <li key={u.email}>{`${u.name} ${u.lastname}: ${u.email}`}</li>
+            ))}
+          </ul>
+        </Card>
+      </Container>
     </div>
   );
 }
-
 export default App;
