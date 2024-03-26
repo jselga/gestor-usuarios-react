255df01ca891b3b23944996c4464756d7af64f04
jselga <jordi.selga@iescarelesvallbona.cat>
2024-03-26 18:30:27 +0100

    Iterando usuario y detalles finales

--- src/App.jsx
+++ src/App.jsx
@@ -5,42 +5,62 @@ import Card from "./components/Card";
 import Container from "./components/Container";
 import Button from "./components/Button"
 function App() {
-  const [formulario, handleChange] = useFormulario({
+  const [usuarios,setUsuarios] =useState([])
+  const [formulario, handleChange,reset] = useFormulario({
     name: "",
     lastname: "",
     email: "",
   });
+  const submit = e =>{
+    
+    e.preventDefault()
+    setUsuarios([
+      ...usuarios,
+      formulario,
+    ])
+    reset()
+  }
 
-  console.log(formulario);
   return (
+    <div style={{marginTop:'15%'}}>
     <Container>
       <Card>
         <div style={{ padding: 20 }}>
-          <form>
+          <form onSubmit={submit}>
             <Input
               label="Nombre"
               name="name"
               value={formulario.name}
               onChange={handleChange}
+              placeholder='Nombre'
             />
             <Input
               label="Apellido"
               name="lastname"
               value={formulario.lastname}
               onChange={handleChange}
+              placeholder='Apellido'
             />
             <Input
               label="Correo"
               name="email"
               value={formulario.email}
               onChange={handleChange}
+              placeholder='Correo'
             />
             <Button>Enviar</Button>
 
           </form>
         </div>
       </Card>
+      <Card>
+        <ul>
+          {usuarios.map(u =>
+            <li key={u.email} >{`${u.name} ${u.lastname}: ${u.email}`}</li> )}
+        </ul>
+      </Card>
     </Container>
+    </div>
   );
 }
 
