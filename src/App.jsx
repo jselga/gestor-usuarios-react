5ff7b2ade0829597bcdf5bacff16eacd6e5b9000
jselga <jordi.selga@iescarelesvallbona.cat>
2024-03-26 18:10:43 +0100

    Componente Button

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import useFormulario from "./hooks/useFormulario";
 import Input from "./components/Input";
 import Card from "./components/Card";
 import Container from "./components/Container";
+import Button from "./components/Button"
 function App() {
-  const [formulario, handleChange] = useFormulario({ name: "", lastname: "" });
+  const [formulario, handleChange] = useFormulario({
+    name: "",
+    lastname: "",
+    email: "",
+  });
 
   console.log(formulario);
   return (
     <Container>
       <Card>
-        <div style={{padding: 20}}>
+        <div style={{ padding: 20 }}>
           <form>
             <Input
               label="Nombre"
@@ -24,6 +29,14 @@ function App() {
               value={formulario.lastname}
               onChange={handleChange}
             />
+            <Input
+              label="Correo"
+              name="email"
+              value={formulario.email}
+              onChange={handleChange}
+            />
+            <Button>Enviar</Button>
+
           </form>
         </div>
       </Card>
